077857ae62c629b16c0bd3750dd11f32a06e0a31
FraMaffezzoli <francesco.maffezzoli@gmail.com>
2024-02-17 11:21:16 +0100

    new version with sample frames

--- scenes/Objects/Update-main-script.gd
+++ scenes/Objects/Update-main-script.gd
@@ -1,29 +1,25 @@
 extends Area2D
 
 onready var Main = 'Empty'
-onready var Cooking = 'Empty'
+onready var Cooking = 'crudo'
 onready var sprite = $AnimatedSprite
 var sprite_frame = null
-var base_path = 'res://asset/'
-var extension = '.png'
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	sprite_frame = base_path + Main + Cooking + '.jpg'
+	sprite_frame = Main
 	sprite.animation = sprite_frame
 	pass # Replace with function body.
 
 func set_property_main(newMain):
 	Main = newMain
-	sprite_frame = base_path + Main + Cooking + extension
+	sprite_frame = Main + Cooking
 	sprite.animation = sprite_frame
 
 func set_property_cooking(newCooking):
 	Cooking = newCooking
-	sprite_frame = base_path + Main + Cooking + extension
+	sprite_frame = Main + Cooking
 	sprite.animation = sprite_frame
-
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
--- scenes/Objects/Update-side-script.gd
+++ scenes/Objects/Update-side-script.gd
@@ -1,19 +1,15 @@
 extends Area2D
 
 onready var sprite = $AnimatedSprite
-var base_path = 'res://asset/'
-var extension = '.png'
-var Side = 'EmptyEmpty'
-var sprite_frame = null
-# Called when the node enters the scene tree for the first time.
+
+onready var sprite_frame = 'Empty'
+# Called when the node enters the scene tree for the first time.nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnny
 func _ready():
-	sprite_frame = base_path + Side + '.jpg'
 	sprite.animation = sprite_frame
 	pass # Replace with function body.
 
 func set_side(newSide):
-	Side = newSide
-	sprite_frame = base_path + Side + '.jpg'
+	sprite_frame = newSide
 	sprite.animation = sprite_frame
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
