211e72757c757e77505449d5346688436e0841ab
Oscar Pindaro <oscar.pindaro@gmail.com>
2024-02-17 18:22:21 +0100

    added good sprites for spawner

--- scenes/ObjectContainers/PrincipalSpawner.gd
+++ scenes/ObjectContainers/PrincipalSpawner.gd
@@ -1,3 +1,4 @@
+tool
 extends StaticBody2D
 
 
@@ -5,22 +6,33 @@ extends StaticBody2D
 # var a = 2
 # var b = "text"
 var item_scene = load("res://scenes/Objects/Item.tscn")
-export var main = ""
+export var main: String = "" setget set_main
+onready var anim_sprite: AnimatedSprite = $AnimatedSprite
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	load_ingredient()
+
+func set_main(new_main):
+	if is_inside_tree():
+		main = new_main
+		load_ingredient()
+
+func load_ingredient():
+	anim_sprite.animation = main
 
 func _player_interact(item: Item) -> Item:
-	if item != null:
+	if not Engine.editor_hint:
+		if item != null:
+			return item
+		# missing code because i need maff code
+		print (item_scene)
+		item = item_scene.instance()
+		item.Main = main
+		# samuele se, era none l'item, deve fare add_child
 		return item
-	# missing code because i need maff code
-	print (item_scene)
-	item = item_scene.instance()
-	item.Main = main
-	# samuele se, era none l'item, deve fare add_child
 	return item
-	
+		
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -28,11 +40,13 @@ func _player_interact(item: Item) -> Item:
 
 
 func _on_Area2D_body_entered(body:Node):
-	if body.is_in_group("player"):
-		body.enter_area(self)
+	if not Engine.editor_hint:
+		if body.is_in_group("player"):
+			body.enter_area(self)
 
 
 
 func _on_Area2D_body_exited(body:Node):
-	if body.is_in_group("player"):
-		body.exit_area(self)
+	if not Engine.editor_hint:
+		if body.is_in_group("player"):
+			body.exit_area(self)
