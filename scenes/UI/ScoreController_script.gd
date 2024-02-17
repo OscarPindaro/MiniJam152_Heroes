e1ed4cffa928ce2592a6464c32feefba2b286530
FraMaffezzoli <francesco.maffezzoli@gmail.com>
2024-02-17 21:19:01 +0100

    connected UI to score

--- scenes/UI/ScoreController_script.gd
+++ scenes/UI/ScoreController_script.gd
@@ -5,6 +5,9 @@ onready var Timer_node = $Timer
 onready var GoldPile_node = $GoldPile
 
 export var score = 10.0
+export var menu_path: NodePath
+
+var Menu_node = null
 var hero_count = 0
 var percent = 0
 # Declare member variables here. Examples:
@@ -14,15 +17,18 @@ var percent = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	Menu_node = get_node(menu_path)
+	Menu_node.connect('change_score', self, '_on_hero_score')
 	Score_node.text = "Coins: " + str(score)
 	GoldPile_node.switch_frame(score)
 	pass # Replace with function body.
+
+
 
 func _on_hero_score(newscore):
 	score += newscore
 	hero_count += 1
 	Score_node.set_text("Coins: " + str(score))
-	ProgressBar_node.Value = score
 	GoldPile_node.set_frame(score)
 	
 
