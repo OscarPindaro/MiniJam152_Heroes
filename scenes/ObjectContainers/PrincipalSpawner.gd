f8f7112fbc5bfd79c0733eada7ea6b4a82383080
Oscar Pindaro <oscar.pindaro@gmail.com>
2024-02-17 19:02:17 +0100

    pngs now are with nearest filte and spawners now have also food on top of it

--- scenes/ObjectContainers/PrincipalSpawner.gd
+++ scenes/ObjectContainers/PrincipalSpawner.gd
@@ -9,14 +9,18 @@ var item_scene = load("res://scenes/Objects/Item.tscn")
 export var main: String = "" setget set_main
 onready var anim_sprite: AnimatedSprite = $AnimatedSprite
 
+
+var main_list = ['bistecca', 'pesce', 'melanzana', 'fungo']
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	load_ingredient()
 
 func set_main(new_main):
 	if is_inside_tree():
-		main = new_main
-		load_ingredient()
+		if new_main in main_list:
+			main = new_main
+			load_ingredient()
 
 func load_ingredient():
 	anim_sprite.animation = main
@@ -29,16 +33,9 @@ func _player_interact(item: Item) -> Item:
 		print (item_scene)
 		item = item_scene.instance()
 		item.Main = main
-		# samuele se, era none l'item, deve fare add_child
 		return item
 	return item
 		
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
 func _on_Area2D_body_entered(body:Node):
 	if not Engine.editor_hint:
 		if body.is_in_group("player"):
--- scenes/ObjectContainers/SideDishSpawner.gd
+++ scenes/ObjectContainers/SideDishSpawner.gd
@@ -1,35 +1,51 @@
+tool
 extends StaticBody2D
 
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-export var side_dish: String = ""
+export var side_dish: String = "" setget set_side_dish
+onready var anim_sprite: AnimatedSprite = $AnimatedSprite
 
+var side_list = ['pomodoro', 'insalata', 'patate']
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	load_ingredient()
+
+func set_side_dish(new_side_dish):
+	if is_inside_tree():
+		if new_side_dish in side_list:
+			side_dish = new_side_dish
+			load_ingredient()
+
+func load_ingredient():
+	anim_sprite.animation = side_dish
 
 func _player_interact(item: Item) -> Item:
-	if item == null:
-		return null
-	if item.Cooking == null:
+	if not Engine.editor_hint:
+		if item == null:
+			return null
+		if item.Cooking == null:
+			return item
+		if item.Side != null:
+			return item
+		item.set_side(side_dish)
 		return item
-	if item.Side != null:
-		return item
-	item.set_side(side_dish)
 	return item
 
 func _on_Area2D_body_entered(body:Node):
-	if body.is_in_group("player"):
-		body.enter_area(self)
+	if not Engine.editor_hint:
+		if body.is_in_group("player"):
+			body.enter_area(self)
 
 
 
 func _on_Area2D_body_exited(body:Node):
-	if body.is_in_group("player"):
-		body.exit_area(self)
+	if not Engine.editor_hint:
+		if body.is_in_group("player"):
+			body.exit_area(self)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
