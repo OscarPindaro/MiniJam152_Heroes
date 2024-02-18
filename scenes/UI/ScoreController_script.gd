73563c08d1bead4f1d51e91f8e84dab2051dcbf3
martinpoles <48521630+martinpoles@users.noreply.github.com>
2024-02-18 15:52:33 +0100

    tip jar sound

--- scenes/UI/ScoreController_script.gd
+++ scenes/UI/ScoreController_script.gd
@@ -14,7 +14,10 @@ var percent = 0
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+var audioInteractionScore
+var makingMoney = preload("res://asset/soundeffects/tip_jar/gold_up.wav")
+var losingMoney = preload("res://asset/soundeffects/tip_jar/gold_down.wav")
+var tempScore
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -22,18 +25,28 @@ func _ready():
 	Menu_node.connect('change_score', self, '_on_hero_score')
 	Score_node.text = "Coins: " + str(score)
 	GoldPile_node.switch_frame(score)
+	audioInteractionScore = get_node("AudioStreamPlayer2D")
+	tempScore = score
 	pass # Replace with function body.
-
-
 
 func _on_hero_score(newscore):
 	score += newscore
 	hero_count += 1
 	Score_node.set_text("Coins: " + str(score))
+	_check_if_sound_and_which()
 	GoldPile_node.switch_frame(score)
 	if score <= 0:
 		get_tree().change_scene(ending_scene)
 
+func _check_if_sound_and_which():
+	var temp = int(tempScore) % 10 == 0	
+	if temp == true:
+		if score > tempScore:
+			audioInteractionScore.set_stream(makingMoney) 
+		else:
+			audioInteractionScore.set_stream(losingMoney) 
+		audioInteractionScore.play()
+		tempScore = score
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
