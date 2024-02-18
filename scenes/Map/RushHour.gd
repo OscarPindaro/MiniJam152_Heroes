68706d1883823c6a8f6abda0d5c19da86af07357
Oscar Pindaro <oscar.pindaro@gmail.com>
2024-02-18 14:50:44 +0100

    changed rush moment for spowner and duration

--- scenes/Map/RushHour.gd
+++ scenes/Map/RushHour.gd
@@ -4,9 +4,12 @@ extends Node2D
 export var rush_hour_emitter_path: NodePath 
 onready var rush_hour_emitter = get_node(rush_hour_emitter_path)
 
+export var flicker_delta: float = 0.1
+export var flicker_duration: float = 2
+
 onready var rush_hour_sprite: Sprite = $RushHourSign
 onready var flicker_timer: Timer = $FlickerTimer
-onready var flicker_timer_process = $FlickerPeriodTimer
+onready var flicker_timer_process: Timer = $FlickerPeriodTimer
 
 var is_flikering = false
 var is_rush : bool = false
@@ -16,6 +19,8 @@ func _ready():
 	rush_hour_emitter.connect("rushHour_start", self, "on_rush_start")
 	rush_hour_emitter.connect("rushHour_end", self, "on_rush_end")
 	rush_hour_sprite.visible = false
+	flicker_timer.wait_time = flicker_delta
+	flicker_timer_process.wait_time = flicker_duration
 
 func on_rush_start():
 	is_rush = true
